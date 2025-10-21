44e77a4b8a931ef0ca9fd8f68a1a61f3e0196900
annsabbasi <annsabbasi54@gmail.com>
2025-10-21 01:56:57 -0700

    first two section responsive done

--- src/pages/Home/components/LandingPage.tsx
+++ src/pages/Home/components/LandingPage.tsx
@@ -4,26 +4,19 @@ import { Star, ArrowRight, ChevronRight } from "lucide-react"
 import dashboardImg from '../../../assets/Home/dashboard-img.webp'
 import dashboardImg2 from '../../../assets/Home/dashboard-i2.png'
 import dashboardImg3 from '../../../assets/Home/dashboard-i3.png'
-import midgradient from '../../../assets/Home/midgradient.png';
 import flash from '../../../assets/Home/flash.png';
 import play from '../../../assets/Home/play.svg';
 import userReview from '../../../assets/Home/users-review.png';
+import DashboardMobile from '../../../assets/Home/Dashboard-Mobile.png';
+import EllipseMobile from '../../../assets/Home/EllipseMobile.svg';
+import SingleBox from '../../../assets/Home/singlebox.png';
 
 function AnimatedGradient() {
     return (
-        <motion.div
-            // className="absolute w-[1500px] h-[600px] top-0 left-1/2 -translate-x-1/2 z-[-1] bg-cover bg-center bg-no-repeat"
-            // className="absolute w-[1500px] h-[600px] top-0 left-1/2 -translate-x-1/2 z-[-1] bg-cover bg-center bg-no-repeat"
-            // style={{
-            //     backgroundImage: `url(${midgradient})`,
-            // }}
-
-            className="absolute inset-0 -z-10 overflow-hidden "
-            style={{
-                backgroundImage: `url(${midgradient})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-            }}
+        <motion.img
+            src={EllipseMobile}
+            alt="Animated gradient background"
+            className="absolute -z-10 w-full h-full scale-150 sm:scale-[120%] translate-y-32 sm:translate-y-0"
             animate={{
                 x: ['-25%', '25%', '-25%'],
             }}
@@ -38,12 +31,12 @@ function AnimatedGradient() {
 
 export function StatBubble() {
     return (
-        <div className="backdrop-blur-sm rounded-3xl px-8 py-4 shadow-[inset_1px_0_1px_rgba(255,255,255,0.1),inset_-1px_0_1px_rgba(255,255,255,0.1)]">
+        <div className="backdrop-blur-sm rounded-[20px] px-8 py-4 shadow-[inset_1px_0_1px_rgba(255,255,255,0.1),inset_-1px_0_1px_rgba(255,255,255,0.1)]">
             <div className="flex items-center gap-4">
-                <img src={flash} alt="light" className="size-10" />
-                <div className="flex text-sm flex-col text-start">
+                <img src={flash} alt="light" className="size:8 sm:size-10" />
+                <div className="flex text:xs sm:text-sm flex-col text-start">
                     <span className="text-muted-foreground text-base">10,000+</span>
-                    <p className="mt-1 text-xs text-muted-foreground">investors sharing insights.</p>
+                    <p className="mt-1 text-[10px] text-muted-foreground">investors sharing insights.</p>
                 </div>
             </div>
         </div>
@@ -52,13 +45,11 @@ export function StatBubble() {
 
 export function TrustBubble() {
     return (
-        // <div className="bg-white/5 backdrop-blur-sm rounded-3xl px-6 py-4 border-r border-l">
-        <div className="backdrop-blur-sm rounded-3xl px-8 py-4 shadow-[inset_1px_0_1px_rgba(255,255,255,0.2),inset_-1px_0_1px_rgba(255,255,255,0.2)]">
+        <div className="backdrop-blur-sm rounded-[20px] px-5 py-4 shadow-[inset_1px_0_1px_rgba(255,255,255,0.2),inset_-1px_0_1px_rgba(255,255,255,0.2)]">
             <div className="flex items-center gap-4">
-                {/* <Zap className="brand" size={16} /> */}
-                <img src={userReview} alt="light" className="w-24" />
+                <img src={userReview} alt="light" className="w-20 sm:w-24" />
                 <div className="flex text-sm flex-col text-start">
-                    <span className="text-gray-300 text-xs">5.0 rating from 1,650+ patients</span>
+                    <span className="text-gray-300 [10px] sm:text-xs">5.0 rating from 1,650+ patients</span>
                     <div className="flex gap-1 items-center">
                         <div className="flex items-center text-amber-400">
                             <Star size={10} fill="currentColor" />
@@ -77,7 +68,7 @@ export function TrustBubble() {
 
 function AppMock() {
     return (
-        <div className="relative">
+        <div className="relative hidden sm:block">
 
             <div className="">
                 <img src={dashboardImg} alt="dashboard" className="w-4/5 mx-auto rounded-4xl border-t border-b border-gray-50/50" />
@@ -94,12 +85,22 @@ function AppMock() {
     )
 }
 
+function MobileMock() {
+    return (
+        <div className="relative block sm:hidden">
+
+            <span className="">
+                <img src={DashboardMobile} alt="dashboard" className="w-[95%] mx-auto" />
+            </span>
+        </div>
+    )
+}
+
 export function Landing() {
     return (
-        // <div className="landing-main landing-gradient relative radial-glow">
         <div className="landing-main relative radial-glow">
             <AnimatedGradient />
-            <div className="z-10 mx-auto max-w-6xl px-4 pt-24 md:pt-36">
+            <div className="relative z-10 mx-auto max-w-6xl px-4 pt-28 sm:pt-24 md:pt-36">
                 <div className="mb-6 mx-auto flex justify-center border w-fit rounded-4xl px-4 py-1 items-center gap-1 cursor-pointer text-sm">
                     🌟 What makes you unique
                     <span className=" ml-3"> <ArrowRight size={16} /> </span>
@@ -108,12 +109,12 @@ export function Landing() {
                 <h1 className="mx-auto max-w-5xl text-center text-balance text-4xl md:text-6xl font-light">
                     Smarter Tech. Smarter Investing.
                 </h1>
-                <p className="mx-auto mt-5 max-w-lg text-center text-gray-300 text-lg font-extralight">
+                <p className="mx-auto mt-3 px-12 sm:px-auto sm:mt-5 max-w-lg text-center text-gray-300 text-lg font-extralight">
                     AI-powered predictions, real-time data, and investor communities — all in one platform.
                 </p>
 
-                <div className="mt-6 flex items-center justify-center gap-3">
-                    <Button className="special-btn hover:scale-110 gradient-box-shadow">
+                <div className="mt-6 flex items-center justify-center sm:flex-row flex-col gap-5 sm:gap-3">
+                    <Button className="special-btn hover:scale-110 gradient-box-shadow w-2/3 sm:w-fit sm:py-5 sm:px-5 py-6 text-base">
                         Start free trial
                         <span className="ml-1">
                             <ChevronRight size={14} />
@@ -121,7 +122,6 @@ export function Landing() {
                     </Button>
 
                     <div className="flex items-center justify-center px-3 rounded-4xl ml-4 hover:scale-110 transition-all">
-                        {/* <CirclePlay className="w-6 h-6 mr-2" /> */}
                         <img src={play} alt="play" className="size-7 mr-2" />
                         <Button variant="outline" className="rounded-full px-0 py-5 !bg-transparent border-none">
                             See demo
@@ -129,18 +129,20 @@ export function Landing() {
                     </div>
                 </div>
 
-                <div className="-mt-6 flex items-center justify-between">
-                    <div className="hidden md:block">
+                <div className="-mt-6 flex items-center justify-center sm:justify-between absolute -bottom-24 sm:-bottom-2 left-1/2 -translate-x-1/2 sm:relative sm:left-auto sm:translate-x-0 w-full">
+                    <div className="hidden sm:block">
                         <StatBubble />
                     </div>
-                    <div className="hidden md:block">
+                    <div className="block">
                         <TrustBubble />
                     </div>
                 </div>
 
-                <div className="mt-8 md:mt-12">
+                <div className="mt-4 md:mt-12">
                     <AppMock />
+                    <MobileMock />
                 </div>
+                <img src={SingleBox} alt="box" className="sm:hidden block absolute top-88 z-[-2] " />
             </div>
 
         </div >
--- src/pages/Home/components/RevolutionPage.tsx
+++ src/pages/Home/components/RevolutionPage.tsx
@@ -1,6 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card"
 import { ArrowRight } from "lucide-react"
-// import gradientGraph from '../../../assets/Home/gradient-graph.png'
 import gradientGraph from '../../../assets/Home/gradient-graph.png'
 import currencyEht from '../../../assets/Home/currency-ethereum.svg'
 import lightBulb from '../../../assets/Home/lightbulb-02.svg'
@@ -12,53 +11,61 @@ import arrowUp from '../../../assets/Home/arrow-up.svg';
 
 export default function RevolutionPage() {
     return (
-        <div className="relative pt-32 pb-5 mx-auto px-6 max-w-6xl">
-            <h1 className="mx-auto max-w-3xl text-center text-balance text-4xl md:text-5xl font-light pb-16">Revolutionizing the
-                Investment worldwide</h1>
+        <div className="relative pt-40 sm:pt-24 pb-5 mx-auto px-4 sm:px-6 max-w-6xl">
+            <h1 className="mx-auto max-w-3xl text-center text-balance text-4xl md:text-5xl font-light sm:pb-16 pb-8">Revolutionizing the Investment worldwide</h1>
 
             <div className="">
-                <img src={singleBox} alt="box" className="absolute -right-[450px] top-10 object-contain w-1/2" />
-                <div className="purple-shadow top-10 -right-[400px]"></div>
+                <img src={singleBox} alt="box" className="absolute  sm:-right-[450px] top-10 object-contain sm:w-1/2  right-0 w-full" />
+                <div className="purple-shadow sm:w-1/2 sm:h-96 top-10 sm:-right-[400px] right-0 w-full h-1/4"></div>
             </div>
 
-            <Card className="relative rounded-4xl bg-[#0B0B0E] border text-white shadow-lg pr-1 pt-1">
+            <Card className="relative rounded-4xl bg-[#0B0B0E] text-white shadow-lg sm:pr-1 pt-1 pr-0">
 
-                <CardContent className="flex flex-col justify-between h-full pr-0">
-                    <div className="flex items-end justify-between">
-                        <p className="text-sm text-gray-400 mb-2 flex items-center gap-2">
+                <CardContent className="flex flex-col justify-between h-full pr-0 sm:pb-6 pb-2 sm:pl-6  pl-0">
+                    <div className="flex items-end justify-between sm:flex-row flex-col">
+                        <p className="text-sm text-gray-400 mb-2 sm:flex items-center gap-2 hidden">
                             <span className="inline-block size-5">
                                 <img src={marketchartGraph} alt="graph" className="opacity-50" />
                             </span>
                             Ask anything
                         </p>
-                        <div className="relative inset-0 w-3/4 -mt-1 -mr-1 overflow-visible">
+                        <div className="relative inset-0 sm:w-3/4 -mt-1 sm:-mr-1 sm:overflow-visible w-full overflow-hidden mr-0">
                             <img
                                 src={gradientGraph}
                                 alt="Financial Graph Background"
-                                className="w-full h-full object-cover ml-auto opacity-70 rounded-tr-4xl"
+                                className="w-full sm:h-full object-cover ml-auto opacity-70 rounded-tr-4xl h-44"
                             />
-                            <div className="absolute top-20 -left-30">
+                            <div className="absolute sm:top-20 sm:-left-30 -left-6 top-28">
                                 <Tag label="Trading strategies" imgVal={lightBulb} />
                             </div>
-                            <div className="absolute -bottom-5 left-30">
+                            <div className="absolute sm:-bottom-5 sm:left-30">
+                                {/* <div className="absolute sm:-bottom-5 sm:left-30  -top-2 right-16"> */}
                                 <Tag label="Risk management" imgVal={sheild} />
                             </div>
-                            <div className="absolute -top-4 right-30">
+                            <div className="absolute sm:-top-4 sm:right-30 bottom-0 -right-24">
                                 <Tag label="Stock and crypto insights" imgVal={currencyEht} />
                             </div>
-                            <div className="absolute -right-10 bottom-10">
+                            <div className="absolute -right-10 bottom-10 sm:block hidden">
                                 <Tag label="Market analysis and trends" imgVal={marketSvg} />
                             </div>
-                            <div className="absolute right-2/6 top-25 w-2/5">
+                            <div className="absolute sm:right-2/6 sm:top-25 sm:w-2/5 w-[60%] top-15 right-[15%]">
                                 <Input label="What can i help you with?" />
                             </div>
                         </div>
                     </div>
-                    <h2 className="text-3xl font-semibold">Financial GPT <ArrowRight className="inline w-6 h-6 ml-1" /></h2>
-                    <p className="text-gray-400 mt-3 max-w-3xl">
-                        Ask me anything about financial markets, trading strategies, stock analysis, economic trends, or investment insights.
-                        I’m here to help you make informed financial decisions.
+                    <p className="text-sm text-gray-400 mb-8 flex items-center gap-2 sm:hidden justify-center sm:mt-0 -mt-6">
+                        <span className="inline-block size-5">
+                            <img src={marketchartGraph} alt="graph" className="opacity-50" />
+                        </span>
+                        Ask anything
                     </p>
+                    <div className="sm:text-left sm:px-0 px-8 text-center">
+                        <h2 className="sm:text-3xl sm:font-semibold text-2xl font-light">Financial GPT <ArrowRight className="inline sm:w-6 sm:h-6 ml-1 w-4 h-4" /></h2>
+                        <p className="text-gray-400 sm:leading-6 mt-3 max-w-3xl sm:text-base text-sm leading-[23px]">
+                            Ask me anything about financial markets, trading strategies, stock analysis, economic trends, or investment insights.
+                            I’m here to help you make informed financial decisions.
+                        </p>
+                    </div>
 
                 </CardContent>
             </Card>
@@ -72,14 +79,13 @@ export default function RevolutionPage() {
 
 export function Tag({ label, imgVal }: { label: string, imgVal: string }) {
     return (
-        <div className="px-6 py-2 bg-[#141418]/70 rounded-full text-sm flex items-center gap-2 border border-white/20 hover:bg-[#1a1a1f]/70 transition-all backdrop-blur-md backdrop-saturate-150">
+        <div className="sm:px-6 sm:py-2 bg-[#141418]/70 rounded-full sm:text-sm flex items-center gap-2 border border-white/20 hover:bg-[#1a1a1f]/70 transition-all backdrop-blur-md backdrop-saturate-150 text-xs px-4 py-1">
             <span className="inline-block">
-                <img src={imgVal} alt="" />
-                {/* {imgVal} */}
+                <img src={imgVal} alt="" className="sm:w-5 sm:h-5 w-4 h-4" />
             </span>
             {label}
             <span>
-                <img src={arrowUp} alt="arrow" className="w-2 h-2 ml-3" />
+                <img src={arrowUp} alt="arrow" className="w-2 h-2 sm:ml-3 ml-1" />
             </span>
         </div>
     )
@@ -88,14 +94,14 @@ export function Tag({ label, imgVal }: { label: string, imgVal: string }) {
 
 export function Input({ label }: { label: string }) {
     return (
-        <div className="glass text-white px-4 py-2 flex items-center justify-between rounded-full">
+        <div className="glass text-white sm:px-4 py-2 flex items-center justify-between rounded-full px-4">
             <input
                 type="text"
                 name="revolution-inp"
                 id="revolution-inp"
                 placeholder={label}
-                className="outline-0 bg-transparent opacity-70 w-full"
-            // disabled
+                className="outline-0 bg-transparent opacity-70 w-full sm:text-base text-sm"
+                disabled
             />
         </div>
     )
